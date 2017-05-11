5133f6c48127bbfbb5f2c19e41e7798df234981d
ezio84 <brabus84@gmail.com>
2017-05-11 12:44:44 +0200

    Launch music player on headset connect: add BT compatibility [2/2]

--- src/com/bootleggers/dumpster/fragments/MiscSettings.java
+++ src/com/bootleggers/dumpster/fragments/MiscSettings.java
@@ -15,6 +15,7 @@ import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
@@ -24,16 +25,31 @@ import com.android.settings.R;
 import java.util.Arrays;
 import java.util.HashSet;
 
+import com.android.settings.Utils;
 import com.android.settings.SettingsPreferenceFragment;
+
 
 public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
+
+    //Launch music player on headset connect - Wireless Compatibility
+    private static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
+    private ListPreference mLaunchPlayerHeadsetConnection;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.bootleg_dumpster_misc);
+        
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mLaunchPlayerHeadsetConnection = (ListPreference) findPreference(HEADSET_CONNECT_PLAYER);
+        int mLaunchPlayerHeadsetConnectionValue = Settings.System.getIntForUser(resolver,
+                Settings.System.HEADSET_CONNECT_PLAYER, 4, UserHandle.USER_CURRENT);
+        mLaunchPlayerHeadsetConnection.setValue(Integer.toString(mLaunchPlayerHeadsetConnectionValue));
+        mLaunchPlayerHeadsetConnection.setSummary(mLaunchPlayerHeadsetConnection.getEntry());
+        mLaunchPlayerHeadsetConnection.setOnPreferenceChangeListener(this);
 
     }
 
@@ -42,9 +58,29 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
         return false;
     }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.DUMPSTER;
+    }
+    
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+    
+        if (preference == mLaunchPlayerHeadsetConnection) {
+            int mLaunchPlayerHeadsetConnectionValue = Integer.valueOf((String) newValue);
+            int index = mLaunchPlayerHeadsetConnection.findIndexOfValue((String) newValue);
+            mLaunchPlayerHeadsetConnection.setSummary(
+                    mLaunchPlayerHeadsetConnection.getEntries()[index]);
+            Settings.System.putIntForUser(resolver, Settings.System.HEADSET_CONNECT_PLAYER,
+                    mLaunchPlayerHeadsetConnectionValue, UserHandle.USER_CURRENT);
+            return true;
+        }
+        return false;
     }
 }
