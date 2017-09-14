df46d8997aa9fa8d726bc76613c13bb742a3517f
Cristiano Matos <cristianobmatos@gmail.com>
2017-09-15 03:18:36 +0530

    RecentsUI: recents panel clear all button tweaks * Show or hide recents button * Place the button in top left, center, right or bottom left, center, right
    Change-Id: Iccb3b1c74da21428dbf63fc9f276d229ada151d1
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/bootleggers/dumpster/fragments/RecentsSettings.java
+++ src/com/bootleggers/dumpster/fragments/RecentsSettings.java
@@ -1,28 +1,48 @@
 package com.bootleggers.dumpster.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
+import android.app.ActivityManagerNative;
 import android.app.Activity;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import android.view.IWindowManager;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import com.android.settings.R;
 
+import java.util.Locale;
 import java.util.Arrays;
 import java.util.HashSet;
+import android.text.TextUtils;
+import android.view.View;
 
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.Utils;
+
 
 public class RecentsSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
+
+    private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+    private ListPreference mRecentsClearAllLocation;
+    private SwitchPreference mRecentsClearAll;
 
     @Override
     public void onCreate(Bundle icicle) {
