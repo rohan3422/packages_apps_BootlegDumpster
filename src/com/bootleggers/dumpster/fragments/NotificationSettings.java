fccbe03ac9770f46dfc87172a98ca99db34676e8
ezio84 <brabus84@gmail.com>
2017-10-16 17:45:42 +0200

    Statusbar notification ticker [2/2]

--- src/com/bootleggers/dumpster/fragments/NotificationSettings.java
+++ src/com/bootleggers/dumpster/fragments/NotificationSettings.java
@@ -33,6 +33,7 @@ public class NotificationSettings extends SettingsPreferenceFragment implements
     private PreferenceCategory mLedsCategory;
     private Preference mChargingLeds;
     private SystemSettingSwitchPreference mLowBatteryBlinking;
+    private ListPreference mTickerMode;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -60,11 +61,28 @@ public class NotificationSettings extends SettingsPreferenceFragment implements
         } else {
             prefSet.removePreference(mLowBatteryBlinking);
         }
-
+        mTickerMode = (ListPreference) findPreference("ticker_mode");
+        mTickerMode.setOnPreferenceChangeListener(this);
+        int tickerMode = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_TICKER,
+                1, UserHandle.USER_CURRENT);
+        mTickerMode.setValue(String.valueOf(tickerMode));
+        mTickerMode.setSummary(mTickerMode.getEntry());
     }
     
-    public boolean onPreferenceChange(Preference preference, Object value) {
-         return true;
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference.equals(mTickerMode)) {
+            int tickerMode = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_TICKER, tickerMode, UserHandle.USER_CURRENT);
+            int index = mTickerMode.findIndexOfValue((String) newValue);
+            mTickerMode.setSummary(
+                    mTickerMode.getEntries()[index]);
+            return true;
+        }
+
+        return false;
     }
 
     @Override
