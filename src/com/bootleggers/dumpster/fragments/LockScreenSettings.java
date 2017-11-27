1a6d19e78c3021adb21c22a4784b7a47891aebbf
Ali B <abittin@gmail.com>
2017-11-28 00:35:03 +0300

    Custom Carrier Label & Carrier Label Placement [2/2]
    PS2 & PS3 Added its own category and moved it to the bottom
    
    Change-Id: Iefcc9949ac4274a5b185567966d4de24bf9713cb

--- src/com/bootleggers/dumpster/fragments/LockScreenSettings.java
+++ src/com/bootleggers/dumpster/fragments/LockScreenSettings.java
@@ -20,8 +20,11 @@ package com.bootleggers.dumpster.fragments;
 import com.android.internal.logging.nano.MetricsProto;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.app.WallpaperManager;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -33,6 +36,9 @@ import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
+import android.text.Spannable;
+import android.text.TextUtils;
+import android.widget.EditText;
 
 import android.provider.Settings;
 import com.android.settings.R;
@@ -40,7 +46,11 @@ import com.android.settings.SettingsPreferenceFragment;
 
 public class LockScreenSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+    
+    private static final String KEY_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
 
+    private Preference mCustomCarrierLabel;
+    private String mCustomCarrierLabelText;
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -50,6 +60,19 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
         final PreferenceScreen prefScreen = getPreferenceScreen();
         Resources resources = getResources();
 
+        mCustomCarrierLabel = (Preference) findPreference(KEY_CUSTOM_CARRIER_LABEL);
+        updateCustomLabelTextSummary();
+    }
+
+    private void updateCustomLabelTextSummary() {
+        mCustomCarrierLabelText = Settings.System.getString(
+                getActivity().getContentResolver(), Settings.System.CUSTOM_CARRIER_LABEL);
+
+        if (TextUtils.isEmpty(mCustomCarrierLabelText)) {
+            mCustomCarrierLabel.setSummary(R.string.custom_carrier_label_notset);
+        } else {
+            mCustomCarrierLabel.setSummary(mCustomCarrierLabelText);
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -58,6 +81,36 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
         return false;
     }
 
+    @Override
+    public boolean onPreferenceTreeClick(final Preference preference) {
+        super.onPreferenceTreeClick(preference);
+        final ContentResolver resolver = getActivity().getContentResolver();
+        if (preference.getKey().equals(KEY_CUSTOM_CARRIER_LABEL)) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+            alert.setTitle(R.string.custom_carrier_label_title);
+            alert.setMessage(R.string.custom_carrier_label_explain);
+
+            // Set an EditText view to get user input
+            final EditText input = new EditText(getActivity());
+            input.setText(TextUtils.isEmpty(mCustomCarrierLabelText) ? "" : mCustomCarrierLabelText);
+            input.setSelection(input.getText().length());
+            alert.setView(input);
+            alert.setPositiveButton(getString(android.R.string.ok),
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            String value = ((Spannable) input.getText()).toString().trim();
+                            Settings.System.putString(resolver, Settings.System.CUSTOM_CARRIER_LABEL, value);
+                            updateCustomLabelTextSummary();
+                            Intent i = new Intent();
+                            i.setAction(Intent.ACTION_CUSTOM_CARRIER_LABEL_CHANGED);
+                            getActivity().sendBroadcast(i);
+                        }
+                    });
+            alert.setNegativeButton(getString(android.R.string.cancel), null);
+            alert.show();
+        }
+        return true;
+    }
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.BOOTLEG;
